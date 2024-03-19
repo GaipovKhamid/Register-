6911f93f54d15e82c94003a2ea08863e1bc969b7
GaipovKhamid <gaipovkhamid65@gmail.com>
2024-03-19 16:34:22 +0500

    Obshiy

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Passwords password = new Passwords();
+        int passw = password.getPassword();
+        String name = password.getName();
+
+        System.out.print("Введите login: ");
+        Scanner login = new Scanner(System.in);
+        String loggin = login.next();
+
+        System.out.print("Введите password: ");
+        Scanner psw = new Scanner(System.in);
+        int pass = psw.nextInt();
+
+        
+    }
+}
---
+++ Passwords.java
@@ -0,0 +1,26 @@
+public class Passwords {
+    public int password=1515;
+    public String name="gaipov";
+
+    public Passwords(int password){
+        this.password = password;
+    }
+
+    public Passwords(){}
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
